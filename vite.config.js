092766a0ef88bf60e6da7cc85d6f750c9608adbb
Novytska12321 <novytska@seznam.cz>
2025-12-17 21:03:12 +0100

    change config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/Lesson1/',
+  base: '/Lesson1/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true
+      }
+    }
+  }
 });
