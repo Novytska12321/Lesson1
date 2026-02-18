d6b1581f2f8fc18d11eb883a3af297ed081811a8
Novytska12321 <novytska@seznam.cz>
2026-02-18 20:05:08 +0100

    navbar

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/Lesson1/',
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 50,
+       awaitWriteFinish: {
+      stabilityThreshold: 50,
+      pollInterval: 50
+    }
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
